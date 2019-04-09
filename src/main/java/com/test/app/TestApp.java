897768c24ad8e8679ec4e463fe3a552226999271
Gupta Mayank <MGupta@corpo.t-mobile.pl>
2019-04-09 11:13:41 +0200

    first commit

---
+++ src/main/java/com/test/app/TestApp.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.test.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author MGupta
+ *
+ */
+@SpringBootApplication
+@RestController
+public class TestApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(TestApp.class, args);
+	}
+	
+	@GetMapping("/testApp")
+	public String test() {
+		System.out.println();
+		return "Hello!!";
+	}
+	
+	@GetMapping("/testApp1")
+	public String test1(@RequestParam(value="name") String name) {
+		return "Hello!!" + name;
+	}
+}
